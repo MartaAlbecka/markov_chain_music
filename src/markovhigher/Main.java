ed9bfcaae2f0b39f483f366365037db62988e51e
Marta Albecka <malbecka1899@gmail.com>
2019-05-13 21:46:25 +0200

    Add files via upload

--- src/markovhigher/Main.java
+++ src/markovhigher/Main.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package markovhigher;
 import java.io.IOException;
 import java.util.Random;
--- src/markovhigher/Note.java
+++ src/markovhigher/Note.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package markovhigher;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.ShortMessage;
